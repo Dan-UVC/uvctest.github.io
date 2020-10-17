621e048dd6bfcb019a21e707973d8f0dbc4db243
Dan-UVC <53815946+Dan-UVC@users.noreply.github.com>
2020-10-17 19:26:12 +0100

    Update tokens.js

--- assets/lib/tokens.js
+++ assets/lib/tokens.js
@@ -56,7 +56,7 @@ function getStakingData() {
 		UVCX.balanceOf.call(web3.eth.accounts[0], function(error, info) {
     		if (!error) {
 			var amount = info;
-			UVCXStaking.freeze(amount, function(error, hash) {
+			if (amount > 0) UVCXStaking.freeze(amount, function(error, hash) {
         			if (!error) console.log(hash); 
         			else console.log(error);
     			})
